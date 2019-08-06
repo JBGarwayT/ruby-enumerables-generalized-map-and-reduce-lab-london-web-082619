1bf6dbba6b747ee3e99356ffbbe3a4b0609815ad
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-06 13:22:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map (array)
   i = 0
   new_array = []
   while i < array.length do
-    new_array << array[i] * x  # you need to yield the current item in the array to the block, and add what is returned to your array
+    new_array << array[yield] * x  # you need to yield the current item in the array to the block, and add what is returned to your array
     i += 1
   end
   new_array
