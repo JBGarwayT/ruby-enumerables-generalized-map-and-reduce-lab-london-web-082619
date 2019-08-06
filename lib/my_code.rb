15a00f102bf6046eb246618840cb2c6c5788cd39
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-06 13:35:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,5 +14,8 @@ def reduce (array, starting_point = 0)
   i = 0
   new_value = starting_point
   while i < array.length do
-    new_value = 
+    new_value += yield(array[i])
+    i += 1
+  end
+end
     
