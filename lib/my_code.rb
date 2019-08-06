e43a0326f84b96e450bf25619e966d002a23e331
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-06 12:54:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
-# Your Code Here
+#my own map
+def map (array)
+  i = 0
+  new_array = []
+  x = yield(array)
+  while i < array.length do
+    new_array << array[i] * x
+    i += 1
+  end
+  new_array
+end
